8baaacbb99bcd06039d280cd197db81d4e740bde
Hardik <hdnakarani@gmail.com>
2019-04-24 13:43:04 +0530

    GET todo listings

--- routes/todos.js
+++ routes/todos.js
@@ -4,7 +4,23 @@ const router = express.Router();
 
 const model = require("../models/index");
 
-router.get("/", (req, res, next) => {});
+//Get todos
+router.get("/", (req, res, next) => {
+  model.Todo.findAll({})
+    .then(todos =>
+      res.json({
+        error: false,
+        data: todos
+      })
+    )
+    .catch(error =>
+      res.json({
+        error: true,
+        data: [],
+        error: error
+      })
+    );
+});
 
 router.post("/", (req, res, next) => {});
 
